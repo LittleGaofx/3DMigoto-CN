4e3680d60181dda7744c69caf31d4e9abbc5ebdb
Little Gao <98456232+LittleGaofx@users.noreply.github.com>
2023-02-12 00:46:38 +0800

    Add files via upload

--- Injector/Injector.cpp
+++ Injector/Injector.cpp
@@ -16,7 +16,7 @@ static void wait_keypress(const char *msg)
 	getchar();
 }
 
-static void wait_exit(int code=0, char *msg="\nPress enter to close...\n")
+static void wait_exit(int code=0, char *msg="\n按Enter以退出程序\n")
 {
 	wait_keypress(msg);
 	exit(code);
@@ -25,9 +25,9 @@ static void wait_exit(int code=0, char *msg="\nPress enter to close...\n")
 static void exit_usage(const char *msg)
 {
 	//                                                          80 column limit --------> \n
-	printf("The Loader is not configured correctly. Please copy the 3DMigoto d3d11.dll\n"
-	       "and d3dx.ini into this directory, then edit the d3dx.ini's [Loader] section\n"
-	       "to set the target executable and 3DMigoto module name.\n"
+	printf("配置错误，请将d3d11.dll复制到目录\n"
+	       "然后编辑d3dx.ini中的[Loader]项目\n"
+	       "以设置目标程序\n"
 	       "\n"
 	       "%s", msg);
 
@@ -48,7 +48,7 @@ static bool check_file_description(const char *buf, const char *module_path)
 	unsigned i;
 
 	if (!VerQueryValueA(buf, "\\VarFileInfo\\Translation", (void**)&translate_query, &query_size))
-		wait_exit(EXIT_FAILURE, "3DMigoto file information query failed\n");
+		wait_exit(EXIT_FAILURE, "文件信息查询失败\n");
 
 	// Look for the 3DMigoto file description in all language blocks... We
 	// could likely skip the loop since we know which language it should be
@@ -60,16 +60,15 @@ static bool check_file_description(const char *buf, const char *module_path)
 				translate_query[i].wLanguage,
 				translate_query[i].wCodePage);
 		if (FAILED(hr))
-			wait_exit(EXIT_FAILURE, "3DMigoto file description query bugged\n");
+			wait_exit(EXIT_FAILURE, "文件信息查询遇到问题\n");
 
 		if (!VerQueryValueA(buf, id, (void**)&file_description, &file_desc_size))
-			wait_exit(EXIT_FAILURE, "3DMigoto file description query failed\n");
+			wait_exit(EXIT_FAILURE, "文件信息查询失败\n");
 
 		// Only look for the 3Dmigoto prefix. We've had a whitespace
 		// error in the description for all this time that we want to
 		// ignore, and we later might want to add other 3DMigoto DLLs
 		// like d3d9 and d3d12 with injection support
-		printf("%s description: \"%s\"\n", module_path, file_description);
 		if (!strncmp(file_description, "3Dmigoto", 8))
 			return true;
 	}
@@ -86,30 +85,30 @@ static void check_3dmigoto_version(const char *module_path, const char *ini_sect
 
 	size = GetFileVersionInfoSizeA(module_path, &pointless_handle);
 	if (!size)
-		wait_exit(EXIT_FAILURE, "3DMigoto version size check failed\n");
+		wait_exit(EXIT_FAILURE, "版本号检查失败\n");
 
 	buf = new char[size];
 
 	if (!GetFileVersionInfoA(module_path, pointless_handle, size, buf))
-		wait_exit(EXIT_FAILURE, "3DMigoto version info check failed\n");
+		wait_exit(EXIT_FAILURE, "版本信息检查失败\n");
 
 	if (!check_file_description(buf, module_path)) {
-		printf("ERROR: The requested module \"%s\" is not 3DMigoto\n"
-		       "Please ensure that [Loader] \"module\" is set correctly and the DLL is in place.", module_path);
+		printf("错误: 模块 \"%s\" 不属于加载器\n"
+		       "请确认 [Loader] \"module\" 设置正确并且DLL文件正确放置", module_path);
 		wait_exit(EXIT_FAILURE);
 	}
 
 	if (!VerQueryValueA(buf, "\\", (void**)&query, &size))
-		wait_exit(EXIT_FAILURE, "3DMigoto version query check failed\n");
+		wait_exit(EXIT_FAILURE, "版本号检查失败\n");
 
-	printf("3DMigoto Version %d.%d.%d\n",
+	printf("3DMigoto版本：%d.%d.%d\nCompile&translate by Little Gao\n\n",
 			query->dwProductVersionMS >> 16,
 			query->dwProductVersionMS & 0xffff,
 			query->dwProductVersionLS >> 16);
 
 	if (query->dwProductVersionMS <  0x00010003 ||
 	    query->dwProductVersionMS == 0x00010003 && query->dwProductVersionLS < 0x000f0000) {
-		wait_exit(EXIT_FAILURE, "This version of 3DMigoto is too old to be safely loaded - please use 1.3.15 or later\n");
+		wait_exit(EXIT_FAILURE, "此版本过旧 - 请使用 1.3.15 或以上版本\n");
 	}
 
 	delete [] buf;
@@ -133,21 +132,21 @@ static bool verify_injection(PROCESSENTRY32 *pe, const wchar_t *module, bool log
 		snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pe->th32ProcessID);
 	} while (snapshot == INVALID_HANDLE_VALUE && GetLastError() == ERROR_BAD_LENGTH);
 	if (snapshot == INVALID_HANDLE_VALUE) {
-		printf("%S (%d): Unable to verify if 3DMigoto was successfully loaded: %d\n",
+		printf("%S (%d): 无法确认3Dmigoto加载是否成功: %d\n",
 				pe->szExeFile, pe->th32ProcessID, GetLastError());
 		return false;
 	}
 
 	me.dwSize = sizeof(MODULEENTRY32);
 	if (!Module32First(snapshot, &me)) {
-		printf("%S (%d): Unable to verify if 3DMigoto was successfully loaded: %d\n",
+		printf("%S (%d): 无法确认3Dmigoto加载是否成功: %d\n",
 				pe->szExeFile, pe->th32ProcessID, GetLastError());
 		goto out_close;
 	}
 
 	// First module is the executable, and this is how we get the full path:
 	if (log_name)
-		printf("Target process found (%i): %S\n", pe->th32ProcessID, me.szExePath);
+		printf("未找到目标程序 (%i): %S\n", pe->th32ProcessID, me.szExePath);
 	wcscpy_s(exe_path, MAX_PATH, me.szExePath);
 
 	rc = false;
@@ -157,7 +156,7 @@ static bool verify_injection(PROCESSENTRY32 *pe, const wchar_t *module, bool log
 
 		if (!_wcsicmp(me.szExePath, module)) {
 			if (!pids.count(pe->th32ProcessID)) {
-				printf("%d: 3DMigoto loaded :)\n", pe->th32ProcessID);
+				printf("%d: 加载成功 :)\n", pe->th32ProcessID);
 				pids.insert(pe->th32ProcessID);
 			}
 			rc = true;
@@ -167,9 +166,9 @@ static bool verify_injection(PROCESSENTRY32 *pe, const wchar_t *module, bool log
 			wcsrchr(mod_path, L'\\')[1] = '\0';
 			if (!_wcsicmp(exe_path, mod_path)) {
 				printf("\n\n\n"
-				       "WARNING: Found a second copy of 3DMigoto loaded from the game directory:\n"
+				       "警告: 在游戏目录下找到重复的3Dmigoto程序:\n"
 				       "%S\n"
-				       "This may crash - please remove the copy in the game directory and try again\n\n\n",
+				       "这可能会导致崩溃 - 请移除游戏目录下重复的3Dmigoto程序\n\n\n",
 				       me.szExePath);
 				wait_exit(EXIT_FAILURE);
 			}
@@ -197,13 +196,13 @@ static bool check_for_running_target(wchar_t *target, const wchar_t *module)
 
 	snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	if (snapshot == INVALID_HANDLE_VALUE) {
-		printf("Unable to verify if 3DMigoto was successfully loaded: %d\n", GetLastError());
+		printf("无法确认3Dmigoto加载是否成功: %d\n", GetLastError());
 		return false;
 	}
 
 	pe.dwSize = sizeof(PROCESSENTRY32);
 	if (!Process32First(snapshot, &pe)) {
-		printf("Unable to verify if 3DMigoto was successfully loaded: %d\n", GetLastError());
+		printf("无法确认3Dmigoto加载是否成功: %d\n", GetLastError());
 		goto out_close;
 	}
 
@@ -233,14 +232,14 @@ static void wait_for_target(const char *target_a, const wchar_t *module_path, bo
 		Sleep(1000);
 
 		if (launched && seconds == 3) {
-			printf("\nStill waiting for the game to start...\n"
-			       "If the game does not launch automatically, leave this window open and run it manually.\n"
-			       "You can also adjust/remove the [Loader] launch= option in the d3dx.ini as desired.\n\n");
+			printf("\n仍在等待游戏启动...\n"
+			       "如果游戏未自动启动，请自行运行游戏程序\n"
+			       "您也可以修改或移除d3dx.ini中的[Loader] launch= 选项\n\n");
 		}
 	}
 
 	for (int i = delay; i > 0; i--) {
-		printf("Shutting down loader in %i...\r", i);
+		printf("加载器将在 %i 后关闭...\r", i);
 		Sleep(1000);
 		check_for_running_target(target_w, module_path);
 	}
@@ -276,8 +275,8 @@ static void elevate_privileges()
 	if (rc > 32) // Success
 		exit(0);
 	if (rc == SE_ERR_ACCESSDENIED)
-		wait_exit(EXIT_FAILURE, "Unable to run as admin: Access Denied\n");
-	printf("Unable to run as admin: %d\n", rc);
+		wait_exit(EXIT_FAILURE, "无法获取管理员权限: 拒绝访问\n");
+	printf("无法获取管理员权限: %d\n", rc);
 	wait_exit(EXIT_FAILURE);
 }
 
@@ -297,7 +296,7 @@ wchar_t* deduce_working_directory(wchar_t *setting, wchar_t dir[MAX_PATH])
 	if (!(ret & FILE_ATTRIBUTE_DIRECTORY) && file_part)
 		*file_part = '\0';
 
-	printf("Using working directory: \"%S\"\n", dir);
+	printf("工作目录位于: \"%S\"\n", dir);
 
 	return dir;
 }
@@ -319,27 +318,27 @@ int main()
 
 	CreateMutexA(0, FALSE, "Local\\3DMigotoLoader");
 	if (GetLastError() == ERROR_ALREADY_EXISTS)
-		wait_exit(EXIT_FAILURE, "ERROR: Another instance of the 3DMigoto Loader is already running. Please close it and try again\n");
+		wait_exit(EXIT_FAILURE, "错误: 检测到另一个3Dmigoto正在运行\n");
 
-	printf("\n------------------------------- 3DMigoto Loader ------------------------------\n\n");
+	printf("\n-------------------------------3DMigoto Loader------------------------------\n\n");
 
 	ini_file = CreateFile(L"d3dx.ini", GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (ini_file == INVALID_HANDLE_VALUE)
-		exit_usage("Unable to open d3dx.ini\n");
+		exit_usage("打开d3dx.ini失败\n");
 
 	filesize = GetFileSize(ini_file, NULL);
 	buf = new char[filesize + 1];
 	if (!buf)
-		wait_exit(EXIT_FAILURE, "Unable to allocate d3dx.ini buffer\n");
+		wait_exit(EXIT_FAILURE, "无法定位d3dx.ini缓存\n");
 
 	if (!ReadFile(ini_file, buf, filesize, &readsize, 0) || filesize != readsize)
-		wait_exit(EXIT_FAILURE, "Error reading d3dx.ini\n");
+		wait_exit(EXIT_FAILURE, "读取d3dx.ini错误\n");
 
 	CloseHandle(ini_file);
 
 	ini_section = find_ini_section_lite(buf, "loader");
 	if (!ini_section)
-		exit_usage("d3dx.ini missing required [Loader] section\n");
+		exit_usage("d3dx.ini 丢失 [Loader]\n");
 
 	// Check that the target is configured. We don't do anything with this
 	// setting from here other than to make sure it is set, because the
@@ -347,10 +346,10 @@ int main()
 	// Once 3DMigoto has been injected it into a process it will check this
 	// value and bail if it is in the wrong one.
 	if (!find_ini_setting_lite(ini_section, "target", target, MAX_PATH))
-		exit_usage("d3dx.ini [Loader] section missing required \"target\" setting\n");
+		exit_usage("d3dx.ini [Loader] 丢失必要设置： \"target\" \n");
 
 	if (!find_ini_setting_lite(ini_section, "module", module_path, MAX_PATH))
-		exit_usage("d3dx.ini [Loader] section missing required \"module\" setting\n");
+		exit_usage("d3dx.ini [Loader] 丢失必要设置： \"module\" \n");
 
 	// We've had support for this injection method in 3DMigoto since 1.3.5,
 	// however until 1.3.15 it lacked the check in DllMain to bail out of
@@ -367,42 +366,41 @@ int main()
 
 	module = LoadLibraryA(module_path);
 	if (!module) {
-		printf("Unable to load 3DMigoto \"%s\"\n", module_path);
+		printf("无法加载3DMigoto \"%s\"\n", module_path);
 		wait_exit(EXIT_FAILURE);
 	}
 
 	GetModuleFileName(module, module_full_path, MAX_PATH);
-	printf("Loaded %S\n\n", module_full_path);
 
 	if (find_ini_setting_lite(ini_section, "entry_point", setting, MAX_PATH))
 		fn = GetProcAddress(module, setting);
 	else
 		fn = GetProcAddress(module, "CBTProc");
 	if (!fn) {
-		wait_exit(EXIT_FAILURE, "Module does not support injection method\n"
-			"Make sure this is a recent 3DMigoto d3d11.dll\n");
+		wait_exit(EXIT_FAILURE, "此模块不支持注入\n"
+			"请确保使用了最新的d3d11.dll\n");
 	}
 
 	hook_proc = find_ini_int_lite(ini_section, "hook_proc", WH_CBT);
 	hook = SetWindowsHookEx(hook_proc, (HOOKPROC)fn, module, 0);
 	if (!hook)
-		wait_exit(EXIT_FAILURE, "Error installing hook\n");
+		wait_exit(EXIT_FAILURE, "hook失败\n");
 
 	rc = EXIT_SUCCESS;
 
 	launch = find_ini_setting_lite(ini_section, "launch", setting, MAX_PATH);
 	if (launch) {
-		printf("3DMigoto ready, launching \"%s\"...\n", setting);
+		printf("3DMigoto 准备就绪, 正在启动 \"%s\"...\n", setting);
 		CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
 
 		if (!MultiByteToWideChar(CP_UTF8, 0, setting, -1, setting_w, MAX_PATH))
-			wait_exit(EXIT_FAILURE, "Invalid launch setting\n");
+			wait_exit(EXIT_FAILURE, "启动设置错误\n");
 
 		working_dir_p = deduce_working_directory(setting_w, working_dir);
 
 		ShellExecute(NULL, NULL, setting_w, NULL, working_dir_p, SW_SHOWNORMAL);
 	} else {
-		printf("3DMigoto ready - Now run the game.\n");
+		printf("3DMigoto准备就绪，请启动游戏\n");
 	}
 
 	wait_for_target(target, module_full_path,
